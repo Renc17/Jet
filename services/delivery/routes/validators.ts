3d4f0618a0e133c313c238bd3cd3aae3cf7862ef
Renc17 <cangaprogram@gmail.com>
2024-04-28 01:20:19 +0300

    feat(delivery): middleware validation

--- services/delivery/routes/validators.ts
+++ services/delivery/routes/validators.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { EstablishmentSchema } from './zod';
+import { CategorySchema, EstablishmentSchema } from './zod';
 
 export const validateCreateEstablishment = (
   req: Request,
@@ -7,6 +7,16 @@ export const validateCreateEstablishment = (
   next: NextFunction
 ) => {
   const { success, error } = EstablishmentSchema.safeParse(req.body);
+  if (!success) return res.status(400).send(error);
+  next();
+};
+
+export const validateCreateMenuCategory = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { success, error } = CategorySchema.safeParse(req.body);
   if (!success) return res.status(400).send(error);
   next();
 };
